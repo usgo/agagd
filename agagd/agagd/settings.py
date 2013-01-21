f5d547e46f388808e72e2eee199e33ee113e2e4d
Andrew Jackson <andrew.m.jackson@gmail.com>
2013-01-21 14:07:14 -0800

    add some template data, add the old CSS file.

--- agagd/agagd/settings.py
+++ agagd/agagd/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/tmp/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,6 +67,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/andrew/work/agagd_project/agagd/static', 
 )
 
 # List of finder classes that know how to find static files in
@@ -106,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.  
-    '/Users/andrew/work/agagd/templates' 
+    '/Users/andrew/work/agagd_project/agagd/templates' 
 )
 
 INSTALLED_APPS = (
