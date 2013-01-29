2aaa80ef47814e27eb3efd2484f3e01118efac46
Andrew Jackson <andrew.m.jackson@gmail.com>
2013-01-28 22:46:09 -0800

    fix vs. table display.

--- agagd/agagd_core/views.py
+++ agagd/agagd_core/views.py
@@ -23,7 +23,7 @@ def member_vs(request, member_id, other_id):
     game_list = Games.objects.filter(
             Q(pin_player_1__exact=member_id, pin_player_2__exact=other_id) |
             Q(pin_player_1__exact=other_id, pin_player_2__exact=member_id),
-            ).order_by('-game_date')[:20]
+            ).order_by('-game_date')
     return render_to_response('agagd_core/member.html',
             {
                 'game_list': game_list,
