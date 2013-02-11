810ce543e5e84aa2db8b4dd3a8a787dc30d9f477
Andrew Jackson <andrew.m.jackson@gmail.com>
2013-02-10 16:06:17 -0800

    Add pagination to pages, fill out the index page with some info.

--- agagd/agagd_core/views.py
+++ agagd/agagd_core/views.py
@@ -2,7 +2,7 @@
 
 from django.template import Context, loader
 from django.shortcuts import render_to_response, render
-from agagd_core.models import Games
+from agagd_core.models import Games, Members
 from agagd_core.tables import GameTable
 from django.http import HttpResponse
 from django.db.models import Q
@@ -11,18 +11,24 @@ from django_tables2   import RequestConfig
 
 
 def index(request):
-    return HttpResponse("foo")
+    game_list = Games.objects.all()
+    table = GameTable(game_list)
+    return render(request, "agagd_core/index.html",
+            {
+                'table': table,
+            })
 
 def member_detail(request, member_id):
     game_list = Games.objects.filter(
             Q(pin_player_1__exact=member_id) | Q(pin_player_2__exact=member_id)
             ).order_by('-game_date')
+    player = Members.objects.filter(member_id=member_id)[0]
     table = GameTable(game_list)
-    RequestConfig(request).configure(table)
+    RequestConfig(request, paginate={"per_page": 20}).configure(table)
     return render(request, 'agagd_core/member.html',
             {
-                'game_list': game_list,
                 'table': table,
+                'player': player,
             }) 
 
 def member_vs(request, member_id, other_id):
@@ -31,10 +37,9 @@ def member_vs(request, member_id, other_id):
             Q(pin_player_1__exact=other_id, pin_player_2__exact=member_id),
             ).order_by('-game_date')
     table = GameTable(game_list)
-    RequestConfig(request).configure(table)
+    RequestConfig(request, paginate={"per_page": 20}).configure(table)
     return render_to_response('agagd_core/member.html',
             {
-                'game_list': game_list,
                 'table': table,
             }) 
 
