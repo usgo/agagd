599a3d9c014599afcba30d8afe68a1c27973276b
Andrew Jackson <andrew.m.jackson@gmail.com>
2013-02-14 15:54:00 -0800

    move db config into respective settings files.  Add a prod.py

--- agagd/agagd/settings/base.py
+++ agagd/agagd/settings/base.py
@@ -9,23 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-try:
-    _password = os.environ['MYSQL_PASS']
-except KeyError:
-    _password = getpass.getpass('Mysql password >')
-    os.environ['MYSQL_PASS'] = _password
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'agagd',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': _password,                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
--- agagd/agagd/settings/prod.py
+++ agagd/agagd/settings/prod.py
@@ -2,13 +2,14 @@
 
 from base import *
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-INSTALLED_APPS += ('django.contrib.admin',)
-
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+try:
+    _user = os.environ['AGAGD_USER']
+except KeyError:
+    _user = getpass.getuser('Mysql user >')
+    os.environ['AGAGD_USER'] = _user
 
 try:
     _password = os.environ['MYSQL_PASS']
@@ -16,11 +17,19 @@ except KeyError:
     _password = getpass.getpass('Mysql password >')
     os.environ['MYSQL_PASS'] = _password
 
+try:
+    _key = os.environ['SECRET_KEY']
+except KeyError:
+    _key = getpass.getpass('random django key >')
+    os.environ['SECRET_KEY'] = _key
+
+SECRET_KEY = _key
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'agagd',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
+        'USER': _user,                      # Not used with sqlite3.
         'PASSWORD': _password,                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
