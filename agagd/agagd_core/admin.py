dea288188211a13258d182d20be71ab58a2f9756
Andrew Jackson <andrew.m.jackson@gmail.com>
2013-02-22 22:02:09 -0800

    remove stale membership references in admin.py

--- agagd/agagd_core/admin.py
+++ agagd/agagd_core/admin.py
@@ -1,13 +1,10 @@
 
-from agagd_core.models import Authteam, Authuser, Changelog, Chapter, Chapters, CommentsAuthors, Country, Games, Members, MembersRegions, Membership  
+from agagd_core.models import Chapter, Chapters, CommentsAuthors, Country, Games, Members, MembersRegions, Membership
 from django.contrib import admin
 
 class MemberAdmin(admin.ModelAdmin): 
     list_display = ('member_id', 'full_name', 'join_date', 'chapter', 'chapter_id')
 
-admin.site.register(Authteam)
-admin.site.register(Authuser)
-admin.site.register(Changelog)
 admin.site.register(Chapter)
 admin.site.register(Chapters)
 admin.site.register(CommentsAuthors)
