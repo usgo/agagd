86fd73aad74bbbeae78bcc564196061853b94816
Andrew Jackson <andrew.m.jackson@gmail.com>
2013-02-23 15:44:41 -0800

    remove 'gd' prefix from urls.  Add a *.php handler to grab old bot requests.

--- agagd/agagd/urls.py
+++ agagd/agagd/urls.py
@@ -10,16 +10,17 @@ urlpatterns = patterns('',
     # url(r'^agagd/', include('agagd.foo.urls')),
     #url(r'^gd/', include('agagd_core.urls')),
 
-     (r'^gd/$', 'agagd_core.views.index'),
-     (r'^gd/member_fetch/$', 'agagd_core.views.member_fetch'),
-     (r'^gd/member_search_form/$', 'agagd_core.views.member_search_form'),
+     (r'^$', 'agagd_core.views.index'),
+     (r'.php$', 'agagd_core.views.index'),
+     (r'^member_fetch/$', 'agagd_core.views.member_fetch'),
+     (r'^member_search_form/$', 'agagd_core.views.member_search_form'),
 
-     (r'^gd/(?P<member_id>\d+)/$', 'agagd_core.views.member_detail'),
-     (r'^gd/(?P<member_id>\d+)/vs/(?P<other_id>\d+)$', 'agagd_core.views.member_vs'), 
-     (r'^gd/ratings/(?P<member_id>\d+)/$', 'agagd_core.views.member_ratings'), 
+     (r'^player/(?P<member_id>\d+)/$', 'agagd_core.views.member_detail'),
+     (r'^player/(?P<member_id>\d+)/vs/(?P<other_id>\d+)$', 'agagd_core.views.member_vs'), 
+     (r'^ratings/(?P<member_id>\d+)/$', 'agagd_core.views.member_ratings'), 
 
-     (r'^gd/tournaments/$', 'agagd_core.views.tournament_list'),
-     (r'^gd/tournaments/(?P<tourn_code>\w{1,20})/$', 'agagd_core.views.tournament_detail'),
+     (r'^tournaments/$', 'agagd_core.views.tournament_list'),
+     (r'^tournaments/(?P<tourn_code>\w{1,20})/$', 'agagd_core.views.tournament_detail'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
