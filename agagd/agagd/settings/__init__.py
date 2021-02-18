320b999f8735b4304102ceb457a1a171b8003579
Michael Hiiva <michaelhiiva@gmail.com>
2021-02-18 02:56:45 +0000

    Removed unused local_settings.

--- agagd/agagd/settings/__init__.py
+++ agagd/agagd/settings/__init__.py
@@ -1,10 +1,3 @@
 # local_settings not required, but makes for a friendlier way to set stage
 import pymysql
 pymysql.install_as_MySQLdb()
-
-try:
-    from local_settings import *
-except ImportError:
-    print( "local_settings not found" )
-    pass
-    
