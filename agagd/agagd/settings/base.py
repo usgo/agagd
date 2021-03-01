9994b4f35cb32bd3d1dfb766085c1b11832b9346
Michael Hiiva <michaelhiiva@gmail.com>
2021-03-01 06:45:14 +0000

    Changed MIDDLEWARE_CLASSES TO MIDDLEWARE for Django 3.0.

--- agagd/agagd/settings/base.py
+++ agagd/agagd/settings/base.py
@@ -71,14 +71,14 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'agagd.urls'
