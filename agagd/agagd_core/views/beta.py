4c83e4d764b5c11ca6a9f9571c8bf1aebced44fe
Michael Hiiva <michaelhiiva@gmail.com>
2021-04-11 21:49:43 -0700

    Changed Model Imports for the AGAGD Index Page.

--- agagd/agagd_core/views/beta.py
+++ agagd/agagd_core/views/beta.py
@@ -1,46 +1,36 @@
+# Datetime Imports
 from datetime import datetime
 from datetime import timedelta
 from datetime import date
-from agagd_core.models import Game
-from agagd_core.models import Member
-from agagd_core.models import Tournament
-from agagd_core.models import TopDan
-from agagd_core.models import TopKyu
-from agagd_core.models import MostRatedGamesPastYear
-from agagd_core.models import MostTournamentsPastYear
-from agagd_core.models import Chapters
-from agagd_core.models import Country
-from agagd_core.tables.beta import GameTable
-from agagd_core.tables.beta import SecondaryGameTable
-from agagd_core.tables.beta import MemberTable
-from agagd_core.tables.beta import ChapterMemberTable
-from agagd_core.tables.beta import TournamentTable
-from agagd_core.tables.beta import OpponentTable
-from agagd_core.tables.beta import TournamentPlayedTable
-from agagd_core.tables.beta import TopDanTable
-from agagd_core.tables.beta import TopKyuTable
-from agagd_core.tables.beta import AllPlayerRatingsTable
-from agagd_core.tables.beta import MostRatedGamesPastYearTable
-from agagd_core.tables.beta import MostTournamentsPastYearTable
+
+# AGAGD Models Import
+import agagd_core.models as agagd_models
+
+# AGAGD Tables Import
+import agagd_core.tables.beta as agagd_tables
+
+# Django Imports
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404 
 from django.shortcuts import redirect
+
+# Django Table Imports
 from django_tables2 import RequestConfig
 
 def index(request):
-    game_list = Game.objects.filter(game_date__gte=datetime.now() - timedelta(days=180)).order_by('-game_date')
-    table = GameTable(game_list, prefix='games')
-    topDanList = TopDan.objects.values()
-    topDanTable = TopDanTable(topDanList)
-    topKyuList = TopKyu.objects.values()
-    topKyuTable = TopKyuTable(topKyuList)
-    mostRatedGamesPastYearList = MostRatedGamesPastYear.objects.values()
-    mostRatedGamesTable = MostRatedGamesPastYearTable(mostRatedGamesPastYearList)
-    mostTournamentsPastYearList = MostTournamentsPastYear.objects.values()
-    mostTournamentsPastYearTable = MostTournamentsPastYearTable(mostTournamentsPastYearList)
+    game_list = agagd_models.Game.objects.filter(game_date__gte=datetime.now() - timedelta(days=180)).order_by('-game_date')
+    table = agagd_tables.GameTable(game_list, prefix='games')
+    topDanList = agagd_models.TopDan.objects.values()
+    topDanTable = agagd_tables.TopDanTable(topDanList)
+    topKyuList = agagd_models.TopKyu.objects.values()
+    topKyuTable = agagd_tables.TopKyuTable(topKyuList)
+    mostRatedGamesPastYearList = agagd_models.MostRatedGamesPastYear.objects.values()
+    mostRatedGamesTable = agagd_tables.MostRatedGamesPastYearTable(mostRatedGamesPastYearList)
+    mostTournamentsPastYearList = agagd_models.MostTournamentsPastYear.objects.values()
+    mostTournamentsPastYearTable = agagd_tables.MostTournamentsPastYearTable(mostTournamentsPastYearList)
     RequestConfig(request).configure(table)
-    tourneys = Tournament.objects.all().order_by('-tournament_date')
-    t_table= TournamentTable(tourneys, prefix='tourneys')
+    tourneys = agagd_models.Tournament.objects.all().order_by('-tournament_date')
+    t_table = agagd_tables.TournamentTable(tourneys, prefix='tourneys')
     RequestConfig(request, paginate={'per_page': 10}).configure(t_table)
 
     return render(request, 'agagd_core/index.beta.html',
