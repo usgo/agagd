59b2bd5c1f1eb82deadd975a2eacb47afd74543b
Michael Hiiva <michaelhiiva@gmail.com>
2021-04-14 00:52:29 -0700

    Updated urls to path and added players path.

--- agagd/agagd_core/urls.py
+++ agagd/agagd_core/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from agagd_core.views import beta as agagd_beta_views
 
-urlpatterns = (url(r"$", agagd_beta_views.index),)
+# Note: Based of request for single qoutes.
+# fmt: off
+urlpatterns = (
+    path('', agagd_beta_views.index),
+    path('players/', agagd_beta_views.list_all_players),
+)
