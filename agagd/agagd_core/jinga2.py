58d6bac6ee47688dcfa8d7ae68da43a3132028c9
Michael Hiiva <michaelhiiva@gmail.com>
2021-05-03 00:16:23 -0700

    Added jinja2 filter for member's name and id.

--- agagd/agagd_core/jinga2.py
+++ agagd/agagd_core/jinga2.py
@@ -1,9 +1,26 @@
+import agagd_core.models as agagd_models
+from django.core.exceptions import ObjectDoesNotExist
 from django.templatetags.static import static
 from django.urls import reverse
 from jinja2 import Environment
 
 
+def get_members_name_and_id(value):
+    """
+        Provides jinja2 custom filter which returns
+        the members information as
+        "[member's full name] (member_id)".
+    """
+    try:
+        member = agagd_models.Member.objects.get(pk=value)
+    except ObjectDoesNotExist:
+        raise ("Member {value} does not exist.")
+
+    return f"{member.full_name} ({member.pk})"
+
+
 def environment(**options):
     env = Environment(**options)
+    env.filters["get_members_name_and_id"] = get_members_name_and_id
     env.globals.update({"static": static, "url": reverse})
     return env
