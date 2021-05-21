33ae7694ba684e052c312437fd2079e6c8a6453c
Michael Hiiva <michaelhiiva@gmail.com>
2021-05-21 15:08:16 -0700

    Added tournament data functions to beta.

--- agagd/agagd_core/views/beta.py
+++ agagd/agagd_core/views/beta.py
@@ -30,6 +30,62 @@ def agagd_paginator_helper(
     return query_list_object_with_page_information
 
 
+def get_opponent_from_game_helper(player_id, game=None):
+    if game.pin_player_1 == player_id:
+        return game.pin_player_1
+    return game.pin_player_2
+
+
+def get_game_date_from_game_helper(game):
+    if game.game_date == "0000-00-00":
+        return None
+    return game.game_date
+
+
+def winner_of_game_helper(game):
+    if game.result == game.color_1:
+        return game.pin_player_1
+    elif game.result == game.color_2:
+        return game.pin_player_2
+    return None
+
+
+def loser_of_game_helper(game):
+    if game.result == game.color_1:
+        return game.pin_player_2
+    elif game.result == game.color_2:
+        return game.pin_player_1
+    return None
+
+
+def tournament_list_helper(player_id, game_list=None):
+    """ Creates a list with the tournament data from the player's games. """
+    tournament_data = {}
+
+    for game in game_list:
+        temp_tournament_data = tournament_data.get(game.tournament_code.pk, {})
+        temp_tournament_data["tournament"] = game.tournament_code
+        temp_tournament_data["won"] = temp_tournament_data.get("won", 0)
+        temp_tournament_data["lost"] = temp_tournament_data.get("lost", 0)
+
+        game_date = get_game_date_from_game_helper(game)
+
+        temp_tournament_data["date"] = temp_tournament_data("date", game_date)
+
+        if winner_of_game_helper(game) == player_id:
+            temp_tournament_data["won"] += 1
+        elif loser_of_game_helper(game) == player_id:
+            temp_tournament_data["lost"] += 1
+
+        tournament_data[game.tournament_code.pk] = temp_tournament_data
+
+    return tournament_data
+
+
+def opponent_list_helper(player_id, game_list=None):
+    """ Creates a list with the opponent data from the player's games. """
+
+
 def index(request):
     default_mobile_column_attrs = "d-none d-lg-table-cell d-xl-table-cell"
 
