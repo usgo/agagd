26f3ef1f345178b4464d65d8222bf5815aadf8fe
Michael Hiiva <michaelhiiva@gmail.com>
2021-05-28 02:13:19 -0700

    Added URL for players.

--- agagd/agagd_core/urls.py
+++ agagd/agagd_core/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 beta_patterns = ([
         path('', beta.index, name='index'),
         path('players/', beta.list_all_players, name='players_list'),
+        path('players/<int:player_id', beta.player_profile, name='player_profile'),
         path('tournaments/', beta.list_all_tournaments, name='tournaments_list'),
         path('tournaments/<slug:code>/', beta.tournament_detail, name='tournament_detail')
     ], 'beta')
