6891130eb9cb2b7c531bad2ede699d1f2cc75d72
Michael Hiiva <michaelhiiva@gmail.com>
2021-06-26 02:35:40 -0700

    Added Some Basic Rules.

---
+++ .agagd_mdlrc_style.rb
@@ -0,0 +1,11 @@
+## Include All Styles
+## https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+all
+
+## General Markdown Exclusions
+
+## Line Length
+exclude_rule 'MD013'
+
+## Bare URL
+exclude_rule 'MD034'
