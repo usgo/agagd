afc22633888f12bfa12d47962df6062d2eb3e6f7
Michael Hiiva <michaelhiiva@gmail.com>
2021-06-26 02:52:26 -0700

    Added Some Markdown Rules

--- .agagd_mdlrc_style.rb
+++ .agagd_mdlrc_style.rb
@@ -2,7 +2,15 @@
 ## https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
 all
 
+## Set Some Rules
+rule 'MD026', punctuation: ',:;'
+
 ## General Markdown Exclusions
+
+## First Line in file should be a top level header
+## Breaks on Github Issue Templates
+exclude_rule 'MD002'
+exclude_rule 'MD041'
 
 ## Line Length
 exclude_rule 'MD013'
