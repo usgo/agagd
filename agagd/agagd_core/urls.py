362e2a74dbd8350e62bb2c7e4f89b192bb77acaf
Michael Hiiva <michaelhiiva@gmail.com>
2021-07-19 06:06:04 -0700

    Added Ratings Overview Page to Beta.

--- agagd/agagd_core/urls.py
+++ agagd/agagd_core/urls.py
@@ -5,6 +5,7 @@ from agagd_core.views.beta.core import (
     tournament_detail,
 )
 from agagd_core.views.beta.index import frontpage
+from agagd_core.views.beta.information import InformationView
 from agagd_core.views.beta.players_profile import players_profile
 
 # Django Imports
@@ -16,6 +17,7 @@ beta_patterns = ([
         path('', frontpage, name='index'),
         path('players/', list_all_players, name='players_list'),
         path('players/<int:player_id>/', players_profile, name='players_profile'),
+        path('information/', InformationView.as_view(), name='ratings_overview'),
         path('tournaments/', list_all_tournaments, name='tournaments_list'),
         path('tournaments/<slug:code>/', tournament_detail, name='tournament_detail')
     ], 'beta')
