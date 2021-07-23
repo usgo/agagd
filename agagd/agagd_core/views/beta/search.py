4064cf441d1e178bf4904a328cf7fc48f208581d
Michael Hiiva <michaelhiiva@gmail.com>
2021-07-23 11:22:00 -0700

    Added Search View for Beta Pages.

--- agagd/agagd_core/views/beta/search.py
+++ agagd/agagd_core/views/beta/search.py
@@ -1,8 +1,23 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 
-class SearchPageView(TemplateView):
+class SearchView(TemplateView):
     template_name = "beta.search_page.html"
+
+    def get(self, request):
+        query = request.GET.get("q", "")
+
+        if not query:
+            return render(request, self.template_name)
+
+        if query.isdigit():
+            member_id = [int(query)]
+            return HttpResponseRedirect(reverse("member_detail", args=member_id))
+
+        return HttpResponseRedirect(f"/search/?q={query}")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
