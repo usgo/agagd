60c6e108c028588cac2a7443b51d4d0c8b2d37e5
Michael Hiiva <michaelhiiva@gmail.com>
2021-09-21 22:54:30 -0700

    Removed Frontpage from Beta.

--- agagd/agagd_core/urls.py
+++ agagd/agagd_core/urls.py
@@ -4,7 +4,6 @@ from agagd_core.views.beta.core import (
     list_all_tournaments,
     tournament_detail,
 )
-from agagd_core.views.beta.index import frontpage
 from agagd_core.views.beta.information import InformationView
 from agagd_core.views.beta.players_profile import players_profile
 from agagd_core.views.beta.qualifications import QualificationsView
@@ -16,7 +15,6 @@ from django.urls import path
 # Note: Based of request for single qoutes.
 # fmt: off
 beta_patterns = ([
-        path('', frontpage, name='index'),
         path('players/', list_all_players, name='players_list'),
         path('players/<int:player_id>/', players_profile, name='players_profile'),
         path('information/', InformationView.as_view(), name='ratings_overview'),
