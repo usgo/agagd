473e937e5c77aa509c85cf1fd460920093792b1e
Michael Hiiva <michaelhiiva@gmail.com>
2021-09-21 22:52:56 -0700

    Removed AGAGD API from Beta Urls.

--- agagd/agagd_core/urls.py
+++ agagd/agagd_core/urls.py
@@ -1,5 +1,4 @@
 # AGAGD Beta Imports
-from agagd_core.views.beta.api import ApiStatusView
 from agagd_core.views.beta.core import (
     list_all_players,
     list_all_tournaments,
@@ -18,7 +17,6 @@ from django.urls import path
 # fmt: off
 beta_patterns = ([
         path('', frontpage, name='index'),
-        path('api/status/', ApiStatusView.as_view(), name='beta_api_status'),
         path('players/', list_all_players, name='players_list'),
         path('players/<int:player_id>/', players_profile, name='players_profile'),
         path('information/', InformationView.as_view(), name='ratings_overview'),
