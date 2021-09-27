e8cb3cee08deea4e3b9c14a8f3c9ee78b9b5187b
Michael Hiiva <michaelhiiva@gmail.com>
2021-09-27 14:54:08 -0700

    Updated RatingsOverviewPage template_name.

--- agagd/agagd_core/views/ratings_overview.py
+++ agagd/agagd_core/views/ratings_overview.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 
 class RatingsOverviewPageView(TemplateView):
-    template_name = "beta.information.html"
+    template_name = "ratings_overview_page.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
