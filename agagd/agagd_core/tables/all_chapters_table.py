de699df1923d1089bb1a75de8813814720636363
Michael Hiiva <michaelhiiva@gmail.com>
2021-09-26 21:02:50 -0700

    All Chapters Page Updated with Configurations and Links.

--- agagd/agagd_core/tables/all_chapters_table.py
+++ agagd/agagd_core/tables/all_chapters_table.py
@@ -1,5 +1,6 @@
 import agagd_core.models as agagd_models
 import django_tables2 as tables
+from django.utils.html import format_html
 
 # Base Bootstrap Column Header Attributes
 default_bootstrap_header_column_attrs = {
@@ -10,7 +11,16 @@ default_bootstrap_header_column_attrs = {
 
 
 class AllChaptersTable(tables.Table):
+    name = tables.Column(linkify=("chapter_detail", [tables.A("member_id")]))
+    url = tables.Column()
+
+    def render_url(self, value):
+        return format_html("<a href='//{}' rel='external'>{}</a>", value, value)
+
     class Meta:
         attrs = default_bootstrap_header_column_attrs
+        fields = ("name", "contact", "meeting_text", "url")
         model = agagd_models.Chapters
+        orderable = False
+        sequence = fields
         template_name = "django_tables2/bootstrap4.html"
